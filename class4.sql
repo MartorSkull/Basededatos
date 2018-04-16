c676c1dddc3273a400c02468930602f54beff80d
MartorSkull <martin_montane_6@hotmail.com>
2018-04-16 15:48:23 -0300

    I uploaded an old version of the file class 4

--- class4.sql
+++ class4.sql
@@ -1,3 +1,4 @@
+USE sakila;
 SELECT title, special_features FROM film WHERE rating="PG-13";
 
 SELECT DISTINCT film.`length` 
@@ -7,8 +8,34 @@ SELECT  title, rental_rate, replacement_cost
 	FROM film 
 		WHERE replacement_cost BETWEEN 20.0 AND 24.0;
 
-SELECT film.title, film_category.category_id,film.rating
+SELECT film.title, category.name,film.rating
 	FROM film, film_category, category
 		WHERE special_features RLIKE 'Behind the Scenes' 
 		AND film.film_id=film_category.film_id
-		AND film_category.category_id=category.category_id;
+		AND film_category.category_id = category.category_id;
+		
+SELECT a.first_name, a.last_name 
+	FROM actor a, film_actor, film 
+		WHERE film_actor.film_id = film.film_id
+		AND film_actor.actor_id = a.actor_id
+		AND film.title = 'ZOOLANDER FICTION';
+		
+SELECT address.address, city.city, country.country
+	FROM address, city, country, store
+		WHERE store.store_id = 1
+		AND store.address_id = address.address_id
+		AND city.city_id = address.city_id
+		AND country.country_id = city.country_id;
+		
+SELECT f1.title, f1.rating, f2.title, f2.rating 
+	FROM film f1, film f2
+		WHERE f1.rating = f2.rating
+		AND f1.film_id != f2.film_id;
+
+SELECT film.title,  count(*) AS quantity, staff.first_name, staff.last_name
+	FROM film, store, inventory, staff
+		WHERE inventory.film_id = film.film_id
+		AND inventory.store_id = 2
+		AND store.store_id = 2
+		AND store.manager_staff_id = staff.staff_id
+	GROUP BY film.title;
