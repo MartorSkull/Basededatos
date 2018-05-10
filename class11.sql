778b22560930d9326f02ea40d5c014088cba0532
MartorSkull <martin_montane_6@hotmail.com>
2018-05-10 14:59:14 -0300

    Class 11

---
+++ class11.sql
@@ -0,0 +1,43 @@
+#1 
+SELECT country.country, count(*) as cities
+FROM country INNER JOIN city USING (country_id)
+GROUP BY country.country_id
+ORDER BY 2;
+
+#2
+SELECT country.country, count(*) as cities
+FROM country INNER JOIN city USING (country_id)
+GROUP BY country.country_id
+HAVING cities > 10
+ORDER BY 2 DESC;
+
+#3
+SELECT CONCAT(customer.last_name, " ", customer.first_name) as name, address.address, COUNT(*) as rented, SUM(payment.amount) as payed
+FROM customer 
+	INNER JOIN address USING (address_id)
+	INNER JOIN rental USING (customer_id)
+	INNER JOIN payment USING (rental_id)
+GROUP BY customer.customer_id
+ORDER BY payed DESC;
+
+#4
+SELECT DISTINCT film.title
+FROM film LEFT JOIN inventory USING (film_id)
+	WHERE inventory.inventory_id IS NULL;
+	
+#5
+SELECT film.title, inventory_id
+FROM film 
+	INNER JOIN inventory USING (film_id)
+	LEFT JOIN rental USING (inventory_id)
+WHERE rental_id IS NULL;
+	
+#6
+SELECT CONCAT(c.last_name, " ",c.first_name) as name, c.store_id, film.title, rental.rental_date, rental.return_date
+FROM rental 
+	INNER JOIN customer c USING (customer_id)
+	INNER JOIN inventory USING (inventory_id)
+	INNER JOIN film USING (film_id)
+ORDER BY store_id, c.last_name;
+
+
--- class6.sql
+++ class6.sql
@@ -11,12 +11,14 @@ SELECT first_name, last_name
 		ORDER by last_name;
 
 #2
+
+#DELETE FROM film_actor WHERE actor_id=148;
+
 SELECT first_name ,last_name
 	FROM actor a1 
-		WHERE NOT EXISTS(
-			SELECT * 
-				FROM film_actor fa, actor a2
-					WHERE fa.actor_id = a2.actor_id);
+		WHERE a1.actor_id NOT IN (
+			SELECT DISTINCT fa.actor_id 
+				FROM film_actor fa);
 			
 
 #3
@@ -49,16 +51,28 @@ SELECT a.actor_id, a.first_name, a.last_name
 				WHERE fa.actor_id = a.actor_id
 				AND f.film_id = fa.film_id
 				AND f.title IN ( 'BETRAYED REAR','CATCH AMISTAD'));
+
+ -- OR --
+
+SELECT DISTINCT actor.actor_id, first_name, last_name
+FROM film_actor, actor, film
+	WHERE actor.actor_id = film_actor.actor_id
+	  AND film.film_id = film_actor.film_id
+	  AND title IN ('BETRAYED REAR', 'CATCH AMISTAD')
+
 #6
 SELECT a.actor_id, a.first_name, a.last_name
 	FROM actor a
-		WHERE EXISTS(
-		SELECT * 
-			FROM film_actor fa, film f 
-				WHERE fa.actor_id = a.actor_id
-				AND f.film_id = fa.film_id
-				AND f.title = 'BETRAYED REAR'
-				AND f.title <> 'CATCH AMISTAD');
+		WHERE EXISTS(SELECT * 
+					 FROM film_actor fa, film f 
+							WHERE fa.actor_id = a.actor_id
+							  AND f.film_id = fa.film_id
+							  AND f.title = 'BETRAYED REAR')
+		  AND NOT EXISTS(SELECT * 
+					 FROM film_actor fa, film f 
+							WHERE fa.actor_id = a.actor_id
+							  AND f.film_id = fa.film_id
+							  AND f.title = 'CATCH AMISTAD');
 #7
 SELECT a.actor_id, a.first_name, a.last_name
 	FROM actor a
@@ -68,7 +82,7 @@ SELECT a.actor_id, a.first_name, a.last_name
 				WHERE fa.actor_id = a.actor_id
 				AND f.film_id = fa.film_id
 				AND f.title = 'BETRAYED REAR')
-		AND EXISTS (		
+		AND NOT EXISTS (		
 		SELECT * 
 			FROM film_actor fa, film f 
 				WHERE fa.actor_id = a.actor_id
